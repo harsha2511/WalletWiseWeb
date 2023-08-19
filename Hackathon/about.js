76c5a3a9acbae3976d76785111691d8df14184f7
harsha2511 <142288212+harsha2511@users.noreply.github.com>
2023-08-19 22:09:52 +0530

    Delete Hackathon directory

--- Hackathon/about.js
+++
@@ -1,6 +0,0 @@
-// Initialize GSAP animations for team members
-const teamMembers = document.querySelectorAll(".team-member");
-
-teamMembers.forEach((member, index) => {
-    gsap.from(member, { opacity: 0, y: 20, duration: 1, delay: 0.2 * index });
-});
